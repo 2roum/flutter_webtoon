729d26d72bbeebc2ea11dc6d7cdba1e884910beb
Toa <ld980105@snu.ac.kr>
2023-03-07 10:53:47 +0900

    :sparkles: build context 이용해서 title 설정
    build context를 이용하여 상위 요소에 접근하여 색상을
    설정함

--- lib/main.dart
+++ lib/main.dart
@@ -1,133 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/button/button.dart';
-import 'package:toonflix/widgets/button/currency_card.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(color: Colors.red),
+          ),
+        ),
         home: Scaffold(
-            backgroundColor: const Color(0xFF181818),
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
+            backgroundColor: const Color(0xFFF4EDDB),
+            body: Center(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 80,
-                    ),
-                    Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Text(
-                            'Hey, Selena',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 28,
-                                fontWeight: FontWeight.w800),
-                          ),
-                          Text(
-                            'Welcome back',
-                            style: TextStyle(
-                                color: Colors.white.withOpacity(0.8),
-                                fontSize: 18),
-                          ),
-                        ],
-                      ),
-                    ]),
-                    const SizedBox(
-                      height: 70,
-                    ),
-                    Text(
-                      'Total Balance',
-                      style: TextStyle(
-                          color: Colors.white.withOpacity(0.8), fontSize: 22),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    const Text(
-                      '\$5 194 482',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 44,
-                          fontWeight: FontWeight.w600),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Button(
-                          text: 'Transfer',
-                          bgColor: Color(0xFFF1B33B),
-                          textColor: Colors.black,
-                        ),
-                        Button(
-                            text: 'Request',
-                            bgColor: Color(0xFF1F2123),
-                            textColor: Colors.white)
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'Wallets',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 36,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        Text(
-                          'View All',
-                          style: TextStyle(
-                              color: Colors.white.withOpacity(0.8),
-                              fontSize: 18),
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const CurrencyCard(
-                      name: 'Euro',
-                      code: 'EUR',
-                      amount: '6 428',
-                      icon: Icons.euro_rounded,
-                      isInverted: false,
-                      order: 0,
-                    ),
-                    const CurrencyCard(
-                      name: 'Bitcoin',
-                      code: 'BTC',
-                      amount: '9 785',
-                      icon: Icons.currency_bitcoin,
-                      isInverted: true,
-                      order: 1,
-                    ),
-                    const CurrencyCard(
-                      name: 'Dollar',
-                      code: 'USD',
-                      amount: '428',
-                      icon: Icons.attach_money_outlined,
-                      isInverted: false,
-                      order: 2,
-                    ),
-                  ],
-                ),
-              ),
-            )));
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyLargeTitle(),
+              ],
+            ))));
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+          fontSize: 30, color: Theme.of(context).textTheme.titleLarge?.color),
+    );
   }
 }
